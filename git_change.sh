7b50d581e1b0db1649cd0b0ed404a2800d9eb0d8
Vegetable_builder <riona1209@outlook.com>
2024-04-04 05:21:28 +0800

    feat:change log

---
+++ git_change.sh
@@ -0,0 +1,12 @@
+git filter-branch --env-filter '
+      if test "$GIT_AUTHOR_EMAIL" = "154750938+Riona1209@users.noreply.github.com"
+      then
+              GIT_AUTHOR_EMAIL=riona1209@outlook.com
+              GIT_AUTHOR_NAME="Vegetable_builder"
+      fi
+      if test "$GIT_COMMITTER_EMAIL" = "1154750938+Riona1209@users.noreply.github.com"
+      then
+              GIT_COMMITTER_EMAIL=riona1209@outlook.com
+              GIT_AUTHOR_NAME="Vegetable_builder"
+      fi
+      ' -- --all
